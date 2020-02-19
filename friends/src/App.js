2527a3a894b61c9d3e9cf6f09036c2bd39de9606
Antoinette <create@aplusmedia.info>
2020-02-18 23:13:10 -0500

    almost there

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import './App.css';
-import {Route} from "react-router";
-import Login from "./components/Login"
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import PrivateRoute from "./components/PrivateRoute";
+
+import FriendsList from "./components/FriendList";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Route path = "/" component={Login} />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/login">LogIn</Link>
+          <Link to="/protected">Protected </Link>
+        </nav>
+
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
+import Friends from "./Friends";
+import FriendsForm from "./FriendsForm";
+
+class FriendsList extends Component {
+  state = {
+    friends: []
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => {
+        console.log(res);
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  addFriend = friend => {
+    axiosWithAuth()
+      .post("/friends", friend)
+
+      .then(res => {
+        
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <FriendsForm addFriend={this.addFriend} />
+        {this.state.friends.map(friend => {
+          return <Friends key={friend.id} data={friend} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default FriendsList;
